78c52324b3cd3b7f1245cf2d16bc3145588d96e1
cosmob3 <3ryanvelasc0@gmail.com>
2025-05-03 22:18:48 -0600

    adding some files to figure out structure of website

---
+++ app/about-me/page.tsx
@@ -0,0 +1,7 @@
+export default function aboutMe() {
+  return (
+    <div>
+      <h1>Hello From about page</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -49,6 +49,12 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <Link
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="../about-me"
+          >
+            about-me
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
