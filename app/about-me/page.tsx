887285014a256a8da7e1c5e5f10e2fb7c8f9d966
cosmob3 <3ryanvelasc0@gmail.com>
2025-05-05 13:18:12 -0600

    making nav bar as a component for webpage

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,7 +1,25 @@
-export default function aboutMe() {
+import NavBar from "../components/navBar/navBar";
+
+export default function aboutPage() {
   return (
     <div>
-      <h1>Hello From about page</h1>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <div className="text-center">
+          <h1 className="flex justify-center text-5xl">Hi, my name is </h1>
+          <h2 className="text-6xl">Bryan Velasco</h2>
+
+          <p className="flex justify-center text-2xl">
+            I am a Data Science student looking to jump into the world of tech!
+          </p>
+          <p className="flex justify-center text-2xl">
+            I have loved computers since I can remember and cannot wait to
+            continue my learning journey in this world of 11001010!
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ app/contact-me/page.tsx
@@ -0,0 +1,18 @@
+import NavBar from "../components/navBar/navBar";
+
+export default function contactMe() {
+  return (
+    <div>
+      <header>
+        <NavBar></NavBar>
+      </header>
+      <main>
+        <article className="text-center">
+          <h1>Welcome to contact page</h1>
+          <p>Here I want to make a form type of thing to demo what I can do</p>
+        </article>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
